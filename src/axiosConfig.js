93ecc9048c3cb17a0835d504cd5be780ef7d3a17
pc24495 <pchummar42@gmail.com>
2022-05-19 23:57:38 -0400

    Changed axios default url back

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -2,9 +2,9 @@ import axios from "axios";
 // Next we make an 'instance' of it
 const instance = axios.create({
   // .. where we make our configurations
-  // baseURL:
-  //   process.env.NODE_ENV === "production" ? "/" : "http://localhost:3000",
-  baseURL: "http://localhost:3000",
+  baseURL:
+    process.env.NODE_ENV === "production" ? "/" : "http://localhost:3000",
+  // baseURL: "http://localhost:3000",
 });
 
 export default instance;
